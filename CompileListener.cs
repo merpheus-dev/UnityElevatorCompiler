6e44850105f0c8a0aaca098f328ad3d6c7ef126d
m3rt32 <mertkirimgeri@gmail.com>
2019-02-04 17:59:13 +0300

    Initial commit

---
+++ CompileListener.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Compilation;
+using System;
+using System.Reflection;
+using System.Media;
+using System.Threading;
+public class CompileListener : EditorWindow
+{
+    static Thread _t;
+    [InitializeOnLoadMethod]
+    public static void Loaded()
+    {
+        CompilationPipeline.assemblyCompilationStarted += CompilationPipeline_assemblyCompilationStarted;
+    }
+
+    private static void CompilationPipeline_assemblyCompilationStarted(string obj)
+    {
+        _t = new Thread(PlaySound)
+        {
+            IsBackground = true
+        };
+        _t.Start();
+    }
+
+    public static void PlaySound()
+    {
+        using (SoundPlayer player = new SoundPlayer(Environment.CurrentDirectory + "/Assets/Editor/elevator.wav"))
+        {
+            player.PlaySync();
+        }
+        _t.Join();
+    }
+}
+
